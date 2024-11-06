bda660be9360d020aea8f4b793464c6043e2972b
Tymon3310 <kopczynski.tymon@gmail.com>
2024-11-06 21:44:37 +0100

    updated configs

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,13 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local api = vim.api
+
+api.nvim_set_keymap("i", "<C-z>", "<Esc>:undo<CR>a", { noremap = true })
+api.nvim_set_keymap("i", "<C-y>", "<Esc>:redo<CR>a", { noremap = true })
+api.nvim_set_keymap("i", "<C-a>", "<Esc>V", { noremap = true })
+
+api.nvim_set_keymap("n", "%", ":source %<CR>", { noremap = true })
+
+api.nvim_set_keymap("c", "<C-l>", "C-u", { noremap = true })
